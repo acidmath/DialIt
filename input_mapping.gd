b891adea1faae3535beaaadb8fe0879349f1aed1
acidmath <adrian.ajx@gmail.com>
2026-01-22 07:01:14 -0600

    a little more

--- input_mapping.gd
+++ input_mapping.gd
@@ -4,7 +4,7 @@ class_name InputMappingMenu
 var isReassigning : bool
 
 func _process(delta: float) -> void:
-	if Input.is_action_pressed("esc"):
+	if Input.is_action_just_released("esc"):
 		if isReassigning:
 			%InputReassignmentPanel.visible = false
 			isReassigning = false
@@ -14,6 +14,12 @@ func _process(delta: float) -> void:
 func return_to_main_menu():
 	get_tree().change_scene_to_file("res://start_menu.tscn")
 
+func show_reassignment_dialog(action : String):
+	if isReassigning:
+		pass
+	isReassigning = true
+	%InputReassignmentPanel.visible = true
+	%InputMappingInstructionsLabel.text = "Please press the button you want to assign to " + action
+
 func thrust_button_pressed():
-	pass
-	#showDialog("thrust")
+	show_reassignment_dialog("thrust")
