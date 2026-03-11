e8943da60c5bbc306e60388049d337686618b948
acidmath <adrian.ajx@gmail.com>
2026-03-10 19:32:37 -0500

    better tracking

--- controls/shifter_0/shifter_0.gd
+++ controls/shifter_0/shifter_0.gd
@@ -5,6 +5,8 @@ extends Node2D
 
 var track_area_entered : bool
 var grab_area_entered : bool
+var initial_shift : bool
+var curr_mouse_position : Vector2
 var last_mouse_position : Vector2
 
 func _ready() -> void:
@@ -15,19 +17,37 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if track_area_entered and grab_area_entered and Input.is_action_pressed("press_down"):
-		var mouse_diff = get_local_mouse_position() - last_mouse_position
-		grab_area.translate(mouse_diff)
-		last_mouse_position = get_local_mouse_position()
+		if !initial_shift:
+			var mouse_diff = curr_mouse_position - last_mouse_position
+			grab_area.translate(mouse_diff)
+		else:
+			initial_shift = false
+		last_mouse_position = curr_mouse_position
+
+func _input(event: InputEvent) -> void:
+	
+	var screen_touch_event : InputEventScreenTouch = event as InputEventScreenTouch
+	if screen_touch_event:
+		curr_mouse_position = screen_touch_event.position
+		return
+	var screen_drag_event : InputEventScreenDrag = event as InputEventScreenDrag
+	if screen_drag_event:
+		curr_mouse_position = screen_drag_event.position
+		return
+	var mouse_event : InputEventMouse = event as InputEventMouse
+	if mouse_event:
+		curr_mouse_position = mouse_event.position
+		return
 
 func on_track_area_entered():
-	last_mouse_position = get_local_mouse_position()
+	initial_shift = grab_area_entered
 	track_area_entered = true
 
 func on_track_area_exited():
 	track_area_entered = false
 
 func on_grab_area_entered():
-	last_mouse_position = get_local_mouse_position()
+	initial_shift = track_area_entered
 	grab_area_entered = true
 
 func on_grab_area_exited():
