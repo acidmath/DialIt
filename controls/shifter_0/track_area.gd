93d64db5a558173e8500a545a8fd5fbc50dd192f
acidmath <adrian.ajx@gmail.com>
2026-03-18 16:55:02 -0500

    shifter background

--- controls/shifter_0/track_area.gd
+++ controls/shifter_0/track_area.gd
@@ -4,12 +4,13 @@ class_name TrackArea
 signal mouse_entered_track
 signal mouse_exited_track
 
-@onready var collision_shape : CollisionPolygon2D = $CollisionPolygon2D
-@onready var drawn_shape : Polygon2D = $CollisionPolygon2D/Polygon2D
+#@onready var collision_shape : CollisionPolygon2D = $CollisionPolygon2D
+#@onready var drawn_shape : Polygon2D = $CollisionPolygon2D/Polygon2D
 
 func _ready() -> void:
-	var collision_polygon : PackedVector2Array = collision_shape.polygon
-	drawn_shape.polygon = collision_polygon
+	#var collision_polygon : PackedVector2Array = collision_shape.polygon
+	#drawn_shape.polygon = collision_polygon
+	pass
 
 func _mouse_enter() -> void:
 	mouse_entered_track.emit()
