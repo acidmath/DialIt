b17b21adb8bf786a109be5ed826a7eda81c936ff
acidmath <adrian.ajx@gmail.com>
2026-03-19 18:28:26 -0500

    clamping more

---
+++ controls/shifter_1/grab_area.gd
@@ -0,0 +1,18 @@
+extends Area2D
+class_name GrabArea
+
+signal mouse_entered_grab
+signal mouse_exited_grab
+
+@onready var collision_shape : CollisionShape2D = $CollisionShape2D
+var collision_shape_radius : float
+
+func _ready() -> void:
+	var collision_shape_shape : CircleShape2D = collision_shape.shape as CircleShape2D
+	collision_shape_radius = collision_shape_shape.radius
+
+func _mouse_enter() -> void:
+	mouse_entered_grab.emit()
+
+func _mouse_exit() -> void:
+	mouse_exited_grab.emit()
