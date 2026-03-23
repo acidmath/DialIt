af8f2aad35c2e2e427c30b5eb472abbe11ca5811
acidmath <adrian.ajx@gmail.com>
2026-03-22 20:26:51 -0500

    bumpers

--- controls/shifter_1/track_area.gd
+++ controls/shifter_1/track_area.gd
@@ -1,19 +1,18 @@
-extends Area2D
-class_name TrackArea
+extends TrackArea
 
-signal mouse_entered_track
-signal mouse_exited_track
-
-@onready var collision_shape : CollisionPolygon2D = $CollisionPolygon2D
+#signal mouse_entered_track
+#signal mouse_exited_track
+#
+#@onready var collision_shape : CollisionPolygon2D = $CollisionPolygon2D
 #@onready var drawn_shape : Polygon2D = $CollisionPolygon2D/Polygon2D
 
-func _ready() -> void:
+#func _ready() -> void:
 	#var collision_polygon : PackedVector2Array = collision_shape.polygon
 	#drawn_shape.polygon = collision_polygon
-	pass
+	#pass
 
-func _mouse_enter() -> void:
-	mouse_entered_track.emit()
-
-func  _mouse_exit() -> void:
-	mouse_exited_track.emit()
+#func _mouse_enter() -> void:
+	#mouse_entered_track.emit()
+#
+#func  _mouse_exit() -> void:
+	#mouse_exited_track.emit()
